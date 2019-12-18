f024c6d72231c28df386bfafc7444f50ea9deb07
Emerson Naranjo <emer.tenor@gmail.com>
2019-12-17 19:02:28 -0500

    End Section 8

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
 
@@ -15,7 +16,7 @@ namespace DatingApp.API.Models
 
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public string KnownAs { get; set; }
         
         public DateTime Created { get; set; }   
@@ -32,6 +33,8 @@ namespace DatingApp.API.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+ 
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
